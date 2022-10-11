6c90bda7e49ae129df9455c4c590906c2af9ad4f
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-10-11 12:06:33 -0300

    usando react bootstrap

--- src/components/artigo.jsx
+++ src/components/artigo.jsx
@@ -1,14 +1,20 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import img from "./img/consultoria-treinamento-mini.jpg";
+
 const Artigo = () => {
   return (
-    <article>
-      <h3>Titulo...</h3>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid iusto
-        consequuntur consectetur qui porro numquam quidem tenetur dolores
-        laboriosam voluptatibus eligendi, vero perferendis earum, distinctio
-        magnam, facilis quo corporis! Rem!
-      </p>
-    </article>
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={img} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
   );
 };
 
