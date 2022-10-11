bc5c25737d4dcaa6df220bd8872ea075715c111c
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-10-11 11:27:22 -0300

    css style components

--- src/components/layout/cabecalho.jsx
+++ src/components/layout/cabecalho.jsx
@@ -1,12 +1,42 @@
+import styled from "styled-components";
 import { Menu } from "../menu";
+
+const periodo = "diurno";
+
+/* Tagged template para uso com styled components `` */
+const StyleCabecalho = styled.header`
+  background-color: ${periodo === "diurno" ? "lightblue" : "darkblue"};
+  text-align: center;
+
+  h1 {
+    color: aliceblue;
+  }
+
+  p {
+    font-weight: bold;
+    color: azure;
+    font-size: 1.5rem;
+
+    &:hover {
+      color: burlywood;
+      background: white;
+      cursor: pointer;
+    }
+  }
+`;
 
 const Cabecalho = () => {
   return (
-    <header>
+    <StyleCabecalho>
       <h1>Interface React</h1>
 
       <Menu />
-    </header>
+
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam
+        libero amet dolore molestiae iusto officiis?
+      </p>
+    </StyleCabecalho>
   );
 };
 
