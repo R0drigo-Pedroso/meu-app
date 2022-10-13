d69c252f7530b91c07565d37e5c70fc794994f50
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-10-13 11:49:42 -0300

    usando o states
    aplicando eventos e

--- src/components/layout/rodape.jsx
+++ src/components/layout/rodape.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
+
 const Rodape = () => {
+  const [estilos, setEstilos] = useState({
+    textAlign: "center",
+    textTransform: "uppercase",
+    border: "solid 2px red",
+  });
+
+  const escoder = () => setEstilos({ display: "none" });
+  const exibir = () => setEstilos({ display: "block" });
+
   return (
-    <footer>
-      <h2>rodapé</h2>
-    </footer>
+    <>
+      <p>
+        <span onMouseOver={escoder}>Esconder</span> |
+        <span onMouseOver={exibir}>Exibir</span>
+      </p>
+      <footer style={estilos}>
+        <h2>rodapé</h2>
+      </footer>
+    </>
   );
 };
 
